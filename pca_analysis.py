baf83514f736341640198713518e024e6368e9f5
Thomas Coudrat <thomas.coudrat@gmail.com>
2015-01-20 14:19:13 +1100

    Using pandas instead of numpy + simplified code

--- pca_analysis.py
+++ pca_analysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import numpy as np
+from __future__ import unicode_literals, print_function
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import argparse
@@ -11,30 +12,26 @@ def main():
     """
     Runs the pca_analysis script
     """
+    # Collect arguments
     csvPath, show_flag, save_flag = parsing()
 
-    # Create the array of data
-    array = np.loadtxt(stripCols(csvPath, 2), delimiter=",")
+    # Prepare the data table
+    df = pd.read_csv(filepath_or_buffer=csvPath, index_col=0, sep=",")
+    ligs = df.index.values
+    colors = df.color.values
+    dfValues = df.drop("color", axis=1)
+    dims = len(dfValues.columns)
 
-    # Get the ligand names for that data
-    ligs = getColumn(csvPath, ",", 0)
-    colors = getColumn(csvPath, ",", 1)
+    # Get the PCA for all dimentions (pc values),
+    # and for the 2 dimentions (graph)
+    X_r, pcValsAll = getPCA(dfValues, dims)
+    X_r, pcVals = getPCA(dfValues, 2)
 
-    # Output info collected
-    displayInfo(array, ligs, colors)
+    # Displaying information to the terminal
+    displayInfo(df, pcValsAll)
 
-    # Get the PCA of that data, with 2 dimensions
-    pca = PCA(n_components=2)
-    X_r = pca.fit(array).transform(array)
-
-    # print pca.explained_variance_ratio_
-    pc1 = str(int(pca.explained_variance_ratio_[0] * 100) / 100.0)
-    pc2 = str(int(pca.explained_variance_ratio_[1] * 100) / 100.0)
-    # print array
-    # print ligs
-    # print X_r
-
-    plotPCA(X_r, pc1, pc2, ligs, colors, csvPath, show_flag, save_flag)
+    # Run the plotting function
+    plotPCA(X_r, pcVals, ligs, colors, csvPath, show_flag, save_flag)
 
 
 def parsing():
@@ -63,28 +60,40 @@ def parsing():
     save_flag = args.save
 
     if not show_flag and not save_flag:
-        print "Select at least one of the flags -show or -save"
+        print("Select at least one of the flags -show or -save")
         sys.exit()
 
     return csvPath, show_flag, save_flag
 
 
-def displayInfo(array, ligs, colors):
+def getPCA(data, dims):
     """
-    Output the information collected to the console
+    Return the PCA data and principal component values given a dataset and a
+    number of dimentions to be returned
     """
-    print "\n## DATA ##"
-    print array
-    print "\n## Names ##"
-    for lig in ligs:
-        print lig
-    print "\n## Colors ##"
-    for color in colors:
-        print color
-    print
+
+    # Get the PCA of that data
+    pca = PCA(n_components=dims)
+    X_r = pca.fit(data).transform(data)
+    pcVals = [str(round(pc, 3)) for pc in pca.explained_variance_ratio_]
+
+    return X_r, pcVals
 
 
-def plotPCA(X_r, pc1, pc2, ligs, colors, csvPath, show_flag, save_flag):
+def displayInfo(df, pcVals):
+    """
+    Print out the information to the terminal
+    """
+
+    print("\n## Data table ##\n")
+    print(df)
+
+    print("\n## Principal Components ##\n")
+    for i, pc in enumerate(pcVals):
+        print("PC" + str(i+1) + " = " + pc)
+
+
+def plotPCA(X_r, pcVals, ligs, colors, csvPath, show_flag, save_flag):
     """
     Plot the PCA data on 2D plot
     """
@@ -95,16 +104,17 @@ def plotPCA(X_r, pc1, pc2, ligs, colors, csvPath, show_flag, save_flag):
 
     for label, col, x, y in zip(ligs, colors, X_r[:, 0], X_r[:, 1]):
         newCol = makeColor(col)
-        ax.scatter(x, y, label=label, color=newCol, marker="o", lw=0, s=800)
+        ax.scatter(x, y, label=label, color=newCol, marker="o", lw=1, s=800)
         # ax.annotate(label, xy=(x, y - 0.05), fontsize=10,
         #             ha='center', va='top')
 
-    ax.set_xlabel("PC1 (" + pc1 + ")", fontsize=30)
-    ax.set_ylabel("PC2 (" + pc2 + ")", fontsize=30)
+    ax.set_xlabel("PC1 (" + pcVals[0] + ")", fontsize=30)
+    ax.set_ylabel("PC2 (" + pcVals[1] + ")", fontsize=30)
     ax.tick_params(axis="both", which="major", labelsize=30)
-    figTitle = "PCA on " + csvPath + " (PC1=" + pc1 + ", PC2=" + pc2 + ")"
-    ax.text(0.5, 1.04, figTitle, horizontalalignment="center", fontsize=30,
-            transform=ax.transAxes)
+    # figTitle = "PCA on " + csvPath + " (PC1=" + pcVals[0] + ", PC2=" +
+    # pcVals[1] + ")"
+    # ax.text(0.5, 1.04, figTitle, horizontalalignment="center", fontsize=30,
+    #         transform=ax.transAxes)
 
     # Legend figure
     fig_legend = plt.figure()
@@ -114,18 +124,14 @@ def plotPCA(X_r, pc1, pc2, ligs, colors, csvPath, show_flag, save_flag):
 
     # Save figures
     if save_flag:
-        print
-        print "SAVING figures"
-        print
+        print("\nSAVING figures\n")
         pngPath = csvPath.replace(".csv", ".png")
         fig.savefig(pngPath, bbox_inches="tight")
         fig_legend.savefig(pngPath.replace(".png", "_legend.png"))
 
     # Show figures
     if show_flag:
-        print
-        print "SHOWING figures"
-        print
+        print("\nSHOWING figures\n")
         plt.show()
 
 
@@ -136,31 +142,6 @@ def makeColor(colorRGB):
     """
     return [float(col)/255. for col in colorRGB.split(":")]
 
-
-def getColumn(csvPath, delimiter, col):
-    """
-    Get only the ligand names from the .csv file
-    """
-    csvFile = open(csvPath, "r")
-    csvLines = csvFile.readlines()
-    csvFile.close()
-
-    ligs = []
-
-    for line in csvLines:
-        ligand = line.split(delimiter)[col]
-        ligs.append(ligand)
-
-    return ligs
-
-
-def stripCols(fname, col, delimiter=","):
-    with open(fname, 'r') as fin:
-        for line in fin:
-            try:
-                yield line.split(delimiter, 2)[col]
-            except IndexError:
-                continue
 
 if __name__ == "__main__":
     main()
